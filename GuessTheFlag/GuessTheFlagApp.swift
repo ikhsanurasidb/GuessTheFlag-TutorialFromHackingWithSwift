2d1cac218e617beab856b45d1abfa866398b7cf2
ikhsanurasidb <muhammad.nurasid@binus.ac.id>
2024-06-28 06:21:32 +0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Muhamamd Nurasid on 28/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
